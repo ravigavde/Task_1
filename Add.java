49b55d4d823761a8d8bbfb446d9585a091e5906e
ravigavde <nightr714@gmail.com>
2020-08-18 09:18:35 +0530

    Added new file for addition
    file created in java

---
+++ Add.java
@@ -0,0 +1,19 @@
+public class Add
+{
+    int add(int a, int b)
+    {
+        return(a+b);
+    }
+    public static void main(String arg[])
+    {
+        int a =0, b=0;
+        Add a = new Add();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter first number ");
+        a = sc.nextInt();
+        System.out.println("Enter second number ");
+        b = sc.nextInt();
+        System.out.println("Addition is "+a.add(a,b));
+    }
+}
